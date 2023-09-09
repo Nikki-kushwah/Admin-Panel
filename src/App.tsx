f2abd9cd643f4e7653c43d80ecfdec4f331846ad
Nikey <nikeykushwah2712@gmail.com>
2023-09-09 20:06:28 +0530

    admin-panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,71 @@
-function App() {
+import Users from "./pages/Users/Users";
+import Home from "./pages/home/Home";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Products from "./pages/products/Products";
+import Navbar from "./Components/navbar/Navbar";
+import Footer from "./Components/footer/Footer";
+import Menu from "./Components/menu/Menu";
+import Login from "./pages/login/Login";
+import "./styles/global.scss";
+import Product from "./pages/product/Product";
+import User from "./pages/user/User";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-  return (
-    <div>Hello World</div>
-  )
+const queryClient = new QueryClient();
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        {
+          path: "/users/:id",
+          element: <User />,
+        },
+        {
+          path: "/products/:id",
+          element: <Product />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
